6b852779186c9c0c3156f57f91fc3ca683efd60d
Lekuro <lekurot@gmail.com>
2020-12-06 04:40:49 +0200

    changed HW3 task2: only numbers

--- HomeWork3/2_number.py
+++ HomeWork3/2_number.py
@@ -3,20 +3,34 @@
 # - Записати число в реверсному порядку.
 # - Посортувати цифри, що входять в дане число
 
-num = 4135261
+num = 4132
 print('number =', num)
 
 # 1
 num_str = str(num)
+digits = []
 product = 1
 for i in num_str:
-    product *= int(i)
+    ii = int(i)
+    digits.append(ii)
+    product *= ii
+# print(digits)
 print(f"The product of digits is {product}.")
 # 2
-print(f'The reversed number is {int(num_str[::-1])}.')
+reversed_digits = digits[:]
+reversed_digits.reverse()
+# print(reversed_digits)
+num_reversed = 0
+len_digits = len(digits)
+for i in range(len_digits):
+    num_reversed += reversed_digits[i]*10**(len_digits-i-1)
+print(f'The reversed number is {num_reversed}.')
 # 3
-str_digits = list(num_str)
-str_digits.sort()
-# print(str_digits)
-str_num_sorted = ''.join(str_digits)
-print(f'The sorted number is {int(str_num_sorted)}.')
+sorted_digits = digits.copy()
+sorted_digits.sort()
+# print(sorted_digits)
+num_sorted = 0
+len_digits = len(digits)
+for i in range(len_digits):
+    num_sorted += sorted_digits[i]*10**(len_digits-i-1)
+print(f'The sorted number is {num_sorted}.')
