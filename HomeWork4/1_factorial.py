5b963de7f7266d699488861bc4cee1280d915f10
Lekuro <lekurot@gmail.com>
2020-12-08 19:09:36 +0200

    changed HW4 task1: added input and if number is negative or zero

--- HomeWork4/1_factorial.py
+++ HomeWork4/1_factorial.py
@@ -1,8 +1,13 @@
 def factorial(x):
     factorial = 1
+    if x == 0 or x == 1:
+        return factorial
+    if x < 0:
+        return 'There is no'
     for i in range(x):
         factorial *= i + 1
     return factorial
 
 
-print(factorial(5))
+number = int(input('Please enter a number: '))
+print(f'Factorial your number is {factorial(number)}.')
