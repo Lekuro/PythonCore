154607917ef9cd09467ccc94e1534652169e7c84
Lekuro <lekurot@gmail.com>
2020-12-08 19:40:39 +0200

    canged TTP4 task1: added comprehension

--- TimeToPractice4/1_even_number.py
+++ TimeToPractice4/1_even_number.py
@@ -1,4 +1,4 @@
-n = 100
+n = 101
 
 for i in range(0, n, 2):
     print(i, end='_')
@@ -8,4 +8,8 @@ i = 0
 while i < n:
     if i % 2 == 0:
         print(i, end='_')
-    i += 2
+    i += 1
+print()
+
+print(list(range(0, n, 2)))
+print([i for i in range(0, n, 2)])
