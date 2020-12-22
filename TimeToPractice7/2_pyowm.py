2ef677e3f7d70949d5ac3c5f6c1d4ec5bb9cb630
Lekuro <lekurot@gmail.com>
2020-12-22 05:16:03 +0200

    added TTP7 T2: pyowm

---
+++ TimeToPractice7/2_pyowm.py
@@ -0,0 +1,28 @@
+# https://github.com/csparpa/pyowm
+# https://home.openweathermap.org/api_keys
+
+from pyowm import OWM
+
+owm = OWM('3908dc3e9c63aee5664b4a811b227153')
+mgr = owm.weather_manager()
+
+city = input('Please enter the city: ')
+
+# Search for current weather in London (Great Britain) and get details
+try:
+    observation = mgr.weather_at_place(city)  # London,GB
+except:
+    print('You enter incorrect data. I take the city: lol. Sorry.')
+    observation = mgr.weather_at_place('lol')
+w = observation.weather
+
+print('clouds:', w.detailed_status)  # 'clouds'
+print('wind:', w.wind())  # {'speed': 4.6, 'deg': 330}
+print('humidity:', w.humidity)  # 87
+# {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+print('temperature:', w.temperature('celsius'))
+# print(w.rain)  # {}
+# print(w.heat_index)  # None
+print('?:', w.clouds)  # 75
+# print(w)
+# print('observation:', observation)
